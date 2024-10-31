f5f6a71b20a65813ffb2de5651b89189a56d5717
Chad Compton <chad@flywithhive.com>
2024-10-31 12:10:44 -0600

    rm hero

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,9 +4,9 @@ export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       {/* <h1 className="mb-4 text-2xl font-bold">Developer Guide</h1> */}
-      <div className="text-center">
+      {/* <div className="text-center">
         <img className="mx-auto max-h-[750px]" src="/imgs/e1evate-hero.png" />
-      </div>
+      </div> */}
       <p className="text-fd-muted-foreground">
         <Link
           href="/docs"
